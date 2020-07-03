354f06e751ff436083481475bdc62f5f506c426e
ruotingw <wruoting@gmail.com>
2020-07-03 18:55:49 -0400

    unit test

--- core/src/test/scala/design/hamu/GameSpec.scala
+++ core/src/test/scala/design/hamu/GameSpec.scala
@@ -1,33 +1,19 @@
 package design.hamu
 
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
 class GameSpec extends AnyWordSpec with Matchers {
-//  "Game with an X on the (0, 0)" should {
-//    val board = Board.initFreshBoard
-//    val game = Game(board, true, 'O')
-//    "let you use 1, 1" in {
-//      b.stringify() must equal("___\n___\n___")
-//    }
-//
-//    val bOneMove = b.move(1, 1, 'X')
-//    "respond to a requested move" in {
-//      bOneMove.stringify() must equal("___\n_X_\n___")
-//    }
-//
-//    "return a requested space" in {
-//      bOneMove.get(1, 1) must equal('X')
-//    }
-//
-//    "respect immutability" in {
-//      b.stringify() must equal("___\n___\n___")
-//    }
-//
-//    val bTwoMove = bOneMove.move(3, -1, 'X')
-//    "gracefully handle an out of bounds move" in {
-//      bTwoMove.stringify() must equal(bOneMove.stringify())
-//    }
-//
-//    "gracefully handle a requested space that is out of bounds" in {
-//      bOneMove.get(-1, 3) must equal('~')
-//    }
-//  }
+  "Game with an X on the (0, 0)" should {
+    val board = Board.initFreshBoard
+    var game = new Game(board, true, 'X')
+    val coordinates = game.convertToCoordinates("4")
+    game = game.input("4")
+    "Coordinates should convert to" in {
+      coordinates must equal((1, 1))
+    }
+    "If a coordinate exists, it should return false" in {
+      game.isMoveValid("4") must equal(false)
+    }
+  }
 }
